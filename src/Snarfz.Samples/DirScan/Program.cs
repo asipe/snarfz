abc5260d053d548984d28f4186d10d37a12f8efe
Andy Sipe <ajs.general@gmail.com>
2013-05-08 07:39:19 -0400

    added directory scan sample

---
+++ src/Snarfz.Samples/DirScan/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Snarfz.Core;
+using SupaCharge.Core.IOAbstractions;
+
+namespace DirScan {
+  internal class Program {
+    private static void Main(string[] args) {
+      try {
+        Execute(args[0]);
+      } catch (Exception ex) {
+        Console.WriteLine(ex);
+      }
+    }
+
+    private static void Execute(string root) {
+      Console.WriteLine("Scanning {0}", root);
+      Console.WriteLine("----");
+
+      var config = new Config(root);
+      config.OnDirectory += (e, a) => Console.WriteLine(a.Directory);
+      new Scanner(new DotNetDirectory()).Start(config);
+    }
+  }
+}
