c913f08200e38620bb8ce6f0d784628d289b9500
Andy Sipe <ajs.general@gmail.com>
2013-05-13 08:40:31 -0400

    updated sample with more configuration options

--- src/Snarfz.Samples/DirScan/Program.cs
+++ src/Snarfz.Samples/DirScan/Program.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Andy Sipe. All rights reserved. Licensed under the MIT License (MIT). See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Snarfz.Core;
 using SupaCharge.Core.IOAbstractions;
 
@@ -8,17 +10,26 @@ namespace DirScan {
   internal class Program {
     private static void Main(string[] args) {
       try {
-        Execute(args[0]);
+        if (args.Length == 0)
+          Console.WriteLine("USAGE: /dir:<root path to scan> /scantype:<All, FilesOnly, DirectoriesOnly> /scanerrormode:<Ask, Throw, Ignore>");
+        Execute(args);
       } catch (Exception ex) {
         Console.WriteLine(ex);
       }
     }
 
-    private static void Execute(string root) {
+    private static void Execute(string[] args) {
+      var root = ExtractArg(args, "/dir:");
+      var scanType = (ScanType)Enum.Parse(typeof(ScanType), ExtractArg(args, "/scantype:"), true);
+      var scanErrorMode = (ScanErrorMode)Enum.Parse(typeof(ScanErrorMode), ExtractArg(args, "/scanerrormode:"), true);
+
       Console.WriteLine("Scanning {0}", root);
       Console.WriteLine("----");
 
-      var config = new Config(root) {ScanErrorMode = ScanErrorMode.Ask};
+      var config = new Config(root) {
+                                      ScanType = scanType,
+                                      ScanErrorMode = scanErrorMode
+                                    };
       config.OnError += (o, a) => {
         Console.WriteLine("!!! ERROR DURING SCAN: " + a.Exception.Message);
         Console.WriteLine("Press Enter To Continue Scan");
@@ -28,6 +39,12 @@ namespace DirScan {
       config.OnFile += (o, a) => Console.WriteLine("     {0}", a.Path);
       new Scanner(new DotNetDirectory(), new ScanErrorHandler()).Start(config);
       Snarfzer.NewScanner().Start(config);
+    }
+
+    private static string ExtractArg(IEnumerable<string> args, string argName) {
+      return args
+        .First(a => a.StartsWith(argName))
+        .Split(new[] {':'}, 2)[1];
     }
   }
 }
