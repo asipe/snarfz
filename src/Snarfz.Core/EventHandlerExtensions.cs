2f848a15956d197bfb19a6c9b94b67524384d79c
Andy Sipe <ajs.general@gmail.com>
2013-05-23 08:09:50 -0400

    added lic header and clean up

--- src/Snarfz.Core/EventHandlerExtensions.cs
+++ src/Snarfz.Core/EventHandlerExtensions.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿// Copyright (c) Andy Sipe. All rights reserved. Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
+using System;
 
 namespace Snarfz.Core {
   public static class EventHandlerExtensions {
-    static public void RaiseEvent<T>(this EventHandler<T> evt, object sender, T args) where T : EventArgs {
+    public static void RaiseEvent<T>(this EventHandler<T> evt, object sender, T args) where T : EventArgs {
       if (evt != null)
         evt(sender, args);
     }
