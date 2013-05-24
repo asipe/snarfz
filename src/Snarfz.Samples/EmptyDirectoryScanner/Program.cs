7d03e9df4b0c29b9073f8f5254005eb36946bf75
Andy Sipe <ajs.general@gmail.com>
2013-05-24 08:05:55 -0400

    added additional sample applications

---
+++ src/Snarfz.Samples/EmptyDirectoryScanner/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Snarfz.Core;
+
+namespace EmptyDirectoryScanner {
+  internal class Program {
+    private static void Main(string[] args) {
+      try {
+        if (args.Length == 0) {
+          Console.WriteLine("USAGE: /dir:<root path to scan>");
+          return;
+        }
+        Execute(args);
+      } catch (Exception ex) {
+        Console.WriteLine(ex);
+      }
+    }
+
+    private static void Execute(IEnumerable<string> args) {
+      var root = ExtractArg(args, "/dir:");
+      Console.WriteLine("Scanning {0}", root);
+      Console.WriteLine("----");
+      var config = new Config(root) {ScanType = ScanType.DirectoriesOnly};
+      config.OnDirectory += ReviewDirectory;
+      Snarfzer.NewScanner().Start(config);
+    }
+
+    private static void ReviewDirectory(object sender, DirectoryVisitEventArgs args) {
+      if ((Directory.GetDirectories(args.Path).Length == 0) && (Directory.GetFiles(args.Path).Length == 0))
+        Console.WriteLine(args.Path);
+    }
+
+    private static string ExtractArg(IEnumerable<string> args, string argName) {
+      return args
+        .First(a => a.StartsWith(argName))
+        .Split(new[] {':'}, 2)[1];
+    }
+  }
+}
---
+++ src/Snarfz.Samples/SimpleScan/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Snarfz.Core;
+
+namespace SimpleScan {
+  internal class Program {
+    private static void Main() {
+      var config = new Config(@"c:\") { ScanErrorMode = ScanErrorMode.Ignore };
+      config.OnDirectory += (o, a) => Console.WriteLine(a.Path);
+      config.OnFile += (o, a) => Console.WriteLine(a.Path);
+      Snarfzer.NewScanner().Start(config);
+    }
+  }
+}
