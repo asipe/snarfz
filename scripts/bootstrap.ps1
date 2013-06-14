0518cc372ba4f1602e7d84566044758038c6737d
Andy Sipe <ajs.general@gmail.com>
2013-06-14 07:51:26 -0400

    updated bootstrap script so nuget packages get pushed to the src\packages folder

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,4 +1,4 @@
-src\.nuget\NuGet.exe install src\.nuget\packages.config -OutputDirectory thirdparty
+src\.nuget\NuGet.exe install src\.nuget\packages.config -OutputDirectory src\packages
 
 $env:PATH += ";.\src\packages\NAnt.Portable.0.92\;.\src\packages\NUnit.Runners.2.6.1\tools"
 
