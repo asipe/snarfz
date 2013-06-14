334ad66a2944343ded740b68086bbdae5f31252e
Andy Sipe <ajs.general@gmail.com>
2013-06-14 07:42:16 -0400

    updated so vs solution and nant both use the same nuget package destinations

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,5 +1,5 @@
 src\.nuget\NuGet.exe install src\.nuget\packages.config -OutputDirectory thirdparty
 
-$env:PATH += ";.\thirdparty\NAnt.Portable.0.92\;.\thirdparty\NUnit.Runners.2.6.1\tools"
+$env:PATH += ";.\src\packages\NAnt.Portable.0.92\;.\src\packages\NUnit.Runners.2.6.1\tools"
 
 nant.exe Init.VSDebug
