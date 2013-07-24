11ec333d1f3b4664672b96de2519db42a9b01e0c
Andy Sipe <ajs.general@gmail.com>
2013-07-24 08:43:27 -0400

    removed nant

--- scripts/setenv.ps1
+++ scripts/setenv.ps1
@@ -1,3 +1,3 @@
 ﻿$ErrorActionPreference = 'Stop'
 
-$env:PATH += ";.\thirdparty\packages\common\NAnt.Portable\;.\thirdparty\packages\common\NUnit.Runners\tools"
+$env:PATH += ";.\thirdparty\packages\common\NUnit.Runners\tools"
