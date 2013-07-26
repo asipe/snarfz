1fbad2be6e51615b339111c20910760a1e741311
Andy Sipe <ajs.general@gmail.com>
2013-07-26 07:59:07 -0400

    fixed case issues

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -13,17 +13,17 @@ if (Test-Path('deploy')) {
 New-Item .\deploy\net-3.5\snarfz.core\merged -ItemType directory -Verbose
 New-Item .\deploy\net-3.5\snarfz.core\raw -ItemType directory -Verbose
 Copy-Item .\debug\net-3.5\snarfz.core\*.* .\deploy\net-3.5\snarfz.core\raw -Verbose
-.\thirdparty\packages\common\ilmerge\ilmerge.exe /t:library /out:.\deploy\net-3.5\snarfz.core\merged\Snarfz.Core.dll /targetplatform:v2 .\deploy\net-3.5\snarfz.core\raw\Snarfz.Core.dll .\deploy\net-3.5\snarfz.core\raw\SupaCharge.Core.dll | Write-Host
+.\thirdparty\packages\common\ilmerge\ilmerge.exe /t:library /out:.\deploy\net-3.5\snarfz.core\merged\snarfz.core.dll /targetplatform:v2 .\deploy\net-3.5\snarfz.core\raw\snarfz.core.dll .\deploy\net-3.5\snarfz.core\raw\supacharge.core.dll | Write-Host
 CheckLastExitCode
 
 New-Item .\deploy\net-4.0\snarfz.core\merged -ItemType directory -Verbose
 New-Item .\deploy\net-4.0\snarfz.core\raw -ItemType directory -Verbose
 Copy-Item .\debug\net-4.0\snarfz.core\*.* .\deploy\net-4.0\snarfz.core\raw -Verbose
-.\thirdparty\packages\common\ilmerge\ilmerge.exe /t:library /out:.\deploy\net-4.0\snarfz.core\merged\Snarfz.Core.dll /targetplatform:v4 .\deploy\net-4.0\snarfz.core\raw\Snarfz.Core.dll .\deploy\net-4.0\snarfz.core\raw\SupaCharge.Core.dll | Write-Host
+.\thirdparty\packages\common\ilmerge\ilmerge.exe /t:library /out:.\deploy\net-4.0\snarfz.core\merged\snarfz.core.dll /targetplatform:v4 .\deploy\net-4.0\snarfz.core\raw\snarfz.core.dll .\deploy\net-4.0\snarfz.core\raw\supacharge.core.dll | Write-Host
 CheckLastExitCode
 
 New-Item .\deploy\net-4.5\snarfz.core\merged -ItemType directory -Verbose
 New-Item .\deploy\net-4.5\snarfz.core\raw -ItemType directory -Verbose
 Copy-Item .\debug\net-4.5\snarfz.core\*.* .\deploy\net-4.5\snarfz.core\raw -Verbose
-.\thirdparty\packages\common\ilmerge\ilmerge.exe /t:library /out:.\deploy\net-4.5\snarfz.core\merged\Snarfz.Core.dll /targetplatform:v4 .\deploy\net-4.5\snarfz.core\raw\Snarfz.Core.dll .\deploy\net-4.5\snarfz.core\raw\SupaCharge.Core.dll | Write-Host
+.\thirdparty\packages\common\ilmerge\ilmerge.exe /t:library /out:.\deploy\net-4.5\snarfz.core\merged\snarfz.core.dll /targetplatform:v4 .\deploy\net-4.5\snarfz.core\raw\snarfz.core.dll .\deploy\net-4.5\snarfz.core\raw\supacharge.core.dll | Write-Host
 CheckLastExitCode
